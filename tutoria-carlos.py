d4eb8f088b80d74bbff493092d4831600ca8a36f
Kaliandra Fortunato Santiago Alves <fortunato.kally88@gmail.com>
2023-03-28 14:10:19 -0300

    primeiro comentario

---
+++ tutoria-carlos.py
@@ -0,0 +1,14 @@
+total_5_centavos = 35 * 0.05
+total_10_centavos = 50 * 0.10
+total_25_centavos = 30 * 0.25
+total_50_centavos = 15 * 0.50
+total_1_real = 19 * 1.00
+total_caixa = (
+    total_5_centavos +
+    total_10_centavos +
+    total_25_centavos +
+    total_50_centavos +
+    total_1_real
+)
+
+print(total_caixa)
